0ba8b24842a265140607d2272d96d28cfee87621
novaardiansyah <novaardiansyah78@gmail.com>
2026-01-19 14:15:10 +0700

    feat(users): implement livewire action for user creation
    - Add random user creation with wire:click action
    - Enable reactive UI updates for user count and list

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -4,31 +4,38 @@
  * Project Name: livewire-course
  * File: Users.php
  * Created Date: Monday January 19th 2026
- * 
+ *
  * Author: Nova Ardiansyah admin@novaardiansyah.id
  * Website: https://novaardiansyah.id
  * MIT License: https://github.com/novaardiansyah/livewire-course/blob/main/LICENSE
- * 
+ *
  * Copyright (c) 2026 Nova Ardiansyah, Org
  */
 
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
+use Pest\Support\Str;
 
 class Users extends Component
 {
   public $modalPluralLabel = 'Users';
   public $users = null;
 
-  public function mount()
-  {
-    $this->users = User::all();
-  }
-
   public function render()
   {
+    $this->users = User::all();
     return view('livewire.users');
+  }
+
+  public function create()
+  {
+    User::create([
+      'name'     => fake()->name(),
+      'email'    => fake()->unique()->safeEmail(),
+      'password' => Hash::make('password'),
+    ]);
   }
 }
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,18 +1,32 @@
 @php
-/*
- * Project Name: livewire-course
- * File: users.blade.php
- * Created Date: Monday January 19th 2026
- * 
- * Author: Nova Ardiansyah admin@novaardiansyah.id
- * Website: https://novaardiansyah.id
- * MIT License: https://github.com/novaardiansyah/livewire-course/blob/main/LICENSE
- * 
- * Copyright (c) 2026 Nova Ardiansyah, Org
- */
+  /*
+   * Project Name: livewire-course
+   * File: users.blade.php
+   * Created Date: Monday January 19th 2026
+   *
+   * Author: Nova Ardiansyah admin@novaardiansyah.id
+   * Website: https://novaardiansyah.id
+   * MIT License: https://github.com/novaardiansyah/livewire-course/blob/main/LICENSE
+   *
+   * Copyright (c) 2026 Nova Ardiansyah, Org
+   */
 @endphp
 
-<div>
+<div class="w-1/2 m-auto my-10">
   <h1 class="text-3xl font-semibold">{{ $modalPluralLabel }}</h1>
   <p>User Count: {{ $users->count() }}</p>
+
+  <button type="button" class="bg-blue-500 hover:bg-blue-600 rounded-lg text-white px-4 py-2 mt-3 cursor-pointer" wire:click="create">
+    Create User
+  </button>
+
+  <hr class="border-1 my-5">
+
+  <h2 class="text-2xl font-semibold">Users List</h2>
+
+  <ul class="list-disc list-inside">
+    @foreach ($users as $user)
+      <li>{{ $user->name }} ({{ $user->email }})</li>
+    @endforeach
+  </ul>
 </div>
